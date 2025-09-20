d11ef1d3cb4ef2994eccbf36940fe27e8f10df2d
Prabhmeetsingh087 <101276967+Prabhmeetsingh087@users.noreply.github.com>
2025-09-20 16:51:58 +0530

    16:49

--- server/controller/create.js
+++ server/controller/create.js
@@ -24,7 +24,7 @@ export const addExpenseFunc = async ( req, res ) => {
 
         // Send a success response
         res.status(200).json({
-            message: 'Expense added successfully testing'
+            message: 'Expense added successfully'
         });
     }
 
