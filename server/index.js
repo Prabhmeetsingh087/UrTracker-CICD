0978b267399d539471fadacca2afc835d4890da0
Prabhmeetsingh087 <101276967+Prabhmeetsingh087@users.noreply.github.com>
2025-09-20 14:57:30 +0530

    14:57

--- server/index.js
+++ server/index.js
@@ -15,7 +15,8 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://urtracker.netlify.app'
+  'https://urtracker.netlify.app',
+  'http://urtracker.s3-website.eu-north-1.amazonaws.com'
 ];
 
 const corsOptions = {
